9dd616f8c362b5f0ce4b036b7d7b95ceaac790ec
Pedro Vieira <pvieira@odysseus.scz.suse.com>
2018-08-04 22:09:12 +0200

    add time stamp to file and remove local directory from search results

--- docu-search.sh
+++ docu-search.sh
@@ -1,33 +1,37 @@
 #!/bin/bash 
 RED='\033[0;31m' # Red color
 NC='\033[0m' # No Color
+DATE=$(date +"%d%m%Y%H%M%S")
+
 echo " "
 printf "${RED}What is the product you want to search? Possible values are listed bellow:${NC}\n"
 printf "${RED}NOTE: You can also use "*" character as a wildcard (example: "sles*" will perform a search on all sles products). The search is case insensitive${NC}\n"
 echo " "
 ls $HOME/Documents/www.suse.com/documentation
 echo " "
-echo "Product:" ; read product
+echo "Product:" ; read PRODUCT
 
 echo " "
 printf "${RED}What are you searching for?${NC}\n"
-echo "Search:" ; read search
+echo "Search:" ; read SEARCH
 
 echo " "
 printf "${RED}RESULTS:${NC}\n"
-grep --color=always -ril --include \*.html "$search" $HOME/Documents/www.suse.com/documentation/$product/singlehtml/ > $HOME/Documents/www.suse.com/docu-search-urls
-urlnumber=$(cat $HOME/Documents/www.suse.com/docu-search-urls |wc -l)
+grep --color=always -ril --include \*.html "$SEARCH" $HOME/Documents/www.suse.com/documentation/$PRODUCT/singlehtml/ > $HOME/Documents/www.suse.com/docu-search-urls
+URLNUMBER=$(cat $HOME/Documents/www.suse.com/docu-search-urls |wc -l)
 
-grep --color=always -ric --include \*.html "$search" $HOME/Documents/www.suse.com/documentation/$product/singlehtml/ |grep -v ^0 > $HOME/Documents/www.suse.com/docu-search-urls-matches
-urlnumbermatches=$(grep -rio --include \*.html "$search" $HOME/Documents/www.suse.com/docu-search-urls-matches |wc -l)
+grep --color=always -ric --include \*.html "$SEARCH" $HOME/Documents/www.suse.com/documentation/$PRODUCT/singlehtml/ |grep -v ^0 > $HOME/Documents/www.suse.com/docu-search-urls-matches
 
-grep --color=always -ri --include \*.html "$search" $HOME/Documents/www.suse.com/documentation/$product/singlehtml/ |sed -e 's/<[^>]*>//g'|tee $HOME/Documents/www.suse.com/docu-search-results |less -R -F -X -I
-#grep --color=always -ri --include \*.html "$search" $HOME/Documents/www.suse.com/documentation/$product/singlehtml/ |grep -v "<[^>]*>" |tee $HOME/Documents/www.suse.com/docu-search-results |less -R -F -X -I 
+grep --color=always -ri --include \*.html "$SEARCH" $HOME/Documents/www.suse.com/documentation/$PRODUCT/singlehtml/ |sed -e 's/<[^>]*>//g' |sed "s|$HOME\/Documents\/||g" |sed 's/html\:/html \:/g' |tee $HOME/Documents/www.suse.com/docu-search-results-$DATE |less -R -F -X -I
+
+#grep --color=always -ri --include \*.html "$SEARCH" $HOME/Documents/www.suse.com/documentation/$PRODUCT/singlehtml/ |grep -v "<[^>]*>" |tee $HOME/Documents/www.suse.com/docu-SEARCH-results |less -R -F -X -I 
 
 echo " "
 printf "${RED}REPORT:${NC}\n"
-echo "The term '"$search"' was found on $urlnumber file(s):"
+echo "The term '"$SEARCH"' was found on $ULRNUMBER file(s):"
 cat $HOME/Documents/www.suse.com/docu-search-urls-matches
 echo " "
-echo "The search results were saved to $HOME/Documents/www.suse.com/docu-search-results"
+
+echo "The search results were saved to $(ls -t $HOME/Documents/www.suse.com/docu-search-results*| head -1)"
+
 
